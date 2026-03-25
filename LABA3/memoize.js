9555006076ddaa3de2b1a88dc0d3e462c84085d9
Vulture <screamzyaka@gmail.com>
2026-03-25 23:33:32 +0200

    implement basic memoization

---
+++ LABA3/memoize.js
@@ -0,0 +1,17 @@
+function memoize(fn) {
+  const cache = new Map();
+
+  return function (...args) {
+    const key = JSON.stringify(args);
+
+    if (cache.has(key)) {
+      console.log('Вилучення з кешу...');
+      return cache.get(key);
+    }
+
+    console.log('Обчислення результату...');
+    const result = fn(...args);
+    cache.set(key, result);
+    return result;
+  };
+}
