05f0b4489bf20be8f3fb367cc25ddbec12f2ba57
Vulture <screamzyaka@gmail.com>
2026-04-04 18:40:54 +0300

    peek method

--- laba4/index.js
+++ laba4/index.js
@@ -1,13 +1,34 @@
 class BiDirectionalPriorityQueue {
   constructor() {
     this.elements = [];
-    this.insertOrderCounter = 0; 
+    this.insertOrderCounter = 0;
   }
   enqueue(item, priority) {
-    this.elements.push({
-      item: item,
-      priority: priority,
-      insertId: this.insertOrderCounter++
-    });
+    this.elements.push({ item, priority, insertId: this.insertOrderCounter++ });
+  }
+  _findIndexByCriteria(criteria) {
+    if (this.elements.length === 0) return -1;
+
+    let targetIndex = 0;
+    for (let i = 1; i < this.elements.length; i++) {
+      const current = this.elements[i];
+      const target = this.elements[targetIndex];
+
+      if (criteria === 'highest' && current.priority > target.priority) {
+        targetIndex = i;
+      } else if (criteria === 'lowest' && current.priority < target.priority) {
+        targetIndex = i;
+      } else if (criteria === 'oldest' && current.insertId < target.insertId) {
+        targetIndex = i;
+      } else if (criteria === 'newest' && current.insertId > target.insertId) {
+        targetIndex = i;
+      }
+    }
+    return targetIndex;
+  }
+  peek(criteria) {
+    const index = this._findIndexByCriteria(criteria);
+    if (index === -1) return null;
+    return this.elements[index].item;
   }
 }
