5e8e1c65dfa4aa42daddd93a10f1a6faefe3b12b
Vulture <screamzyaka@gmail.com>
2026-04-05 14:57:06 +0300

    dequeue method

--- laba4/index.js
+++ laba4/index.js
@@ -3,32 +3,36 @@ class BiDirectionalPriorityQueue {
     this.elements = [];
     this.insertOrderCounter = 0;
   }
+
   enqueue(item, priority) {
     this.elements.push({ item, priority, insertId: this.insertOrderCounter++ });
   }
+
   _findIndexByCriteria(criteria) {
     if (this.elements.length === 0) return -1;
-
     let targetIndex = 0;
     for (let i = 1; i < this.elements.length; i++) {
       const current = this.elements[i];
       const target = this.elements[targetIndex];
 
-      if (criteria === 'highest' && current.priority > target.priority) {
-        targetIndex = i;
-      } else if (criteria === 'lowest' && current.priority < target.priority) {
-        targetIndex = i;
-      } else if (criteria === 'oldest' && current.insertId < target.insertId) {
-        targetIndex = i;
-      } else if (criteria === 'newest' && current.insertId > target.insertId) {
-        targetIndex = i;
-      }
+      if (criteria === 'highest' && current.priority > target.priority) targetIndex = i;
+      else if (criteria === 'lowest' && current.priority < target.priority) targetIndex = i;
+      else if (criteria === 'oldest' && current.insertId < target.insertId) targetIndex = i;
+      else if (criteria === 'newest' && current.insertId > target.insertId) targetIndex = i;
     }
     return targetIndex;
   }
+
   peek(criteria) {
     const index = this._findIndexByCriteria(criteria);
     if (index === -1) return null;
     return this.elements[index].item;
+  }
+  
+  dequeue(criteria) {
+    const index = this._findIndexByCriteria(criteria);
+    if (index === -1) return null;
+    const removedElement = this.elements.splice(index, 1)[0];
+    return removedElement.item;
   }
 }
